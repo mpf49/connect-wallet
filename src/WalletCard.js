f3da7ae580ef4f0b347b9dbbfcd5f07765b57db4
mpf49 <110920099+mpf49@users.noreply.github.com>
2022-08-22 17:09:04 +0400

    bal and account

--- src/WalletCard.js
+++ src/WalletCard.js
@@ -1,37 +1,75 @@
 import React, {useState} from 'react'
-import './walletStyle.css';
+import {ethers} from 'ethers'
+import './walletStyle.css'
+
+const WalletCard = () => {
+
+	const [errorMessage, setErrorMessage] = useState(null);
+	const [defaultAccount, setDefaultAccount] = useState(null);
+	const [userBalance, setUserBalance] = useState(null);
+	const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+
+	const connectWalletHandler = () => {
+		if (window.ethereum && window.ethereum.isMetaMask) {
+			console.log('MetaMask Here!');
+
+			window.ethereum.request({ method: 'eth_requestAccounts'})
+			.then(result => {
+				accountChangedHandler(result[0]);
+				setConnButtonText('Wallet Connected');
+				getAccountBalance(result[0]);
+			})
+			.catch(error => {
+				setErrorMessage(error.message);
+			
+			});
+
+		} else {
+			console.log('Need to install MetaMask');
+			setErrorMessage('Please install MetaMask browser extension to interact');
+		}
+	}
+
+	
+	const accountChangedHandler = (newAccount) => {
+		setDefaultAccount(newAccount);
+		getAccountBalance(newAccount.toString());
+	}
+
+	const getAccountBalance = (account) => {
+		window.ethereum.request({method: 'eth_getBalance', params: [account, 'latest']})
+		.then(balance => {
+			setUserBalance(ethers.utils.formatEther(balance));
+		})
+		.catch(error => {
+			setErrorMessage(error.message);
+		});
+	};
+
+	const chainChangedHandler = () => {
+		
+		window.location.reload();
+	}
 
 
+	
+	window.ethereum.on('accountsChanged', accountChangedHandler);
 
-const WallerCard = () => {
-    const [errorMessage, setErrorMessage] = useState(null);
-    const [defaultAccount, setDefaultAccount] = useState(null);
-    const [connButtonText, setConnectText] = useState('Connect Wallet');
-    const connectWalletHandler = () => {
-        if (window.ethereum) {
-            window.ethereum.request({method: 'eth_requestAccounts'})
-            .then(result => {
-                accountChangedHandler(result[0]);
-            })
-        } else {
-            setErrorMessage('Install MetaMask');
-        }
-    }
-    const accountChangedHandler = (newAccount) => {
-           setDefaultAccount(newAccount);
-    }
-    
-    return (
-        <div className='walletCard'>
-            <h4 className='headWallet'> {"MetaMask Wallet"}
-         </h4>
-         <button className='MetaBtn' onClick={connectWalletHandler}>{connButtonText}</button>
-         <div className='accountDisplay'>
-         <h3>Address: {defaultAccount}</h3>
-         </div>
-         {errorMessage}
-        </div>
-    )
-    }
- 
-export default WallerCard;
+	window.ethereum.on('chainChanged', chainChangedHandler);
+	
+	return (
+		<div className='walletCard'>
+		<h4> {"Metamask Wallet"} </h4>
+			<button className='MetaBtn' onClick={connectWalletHandler}>{connButtonText}</button>
+			<div className='accountDisplay'>
+				<h3>Address: {defaultAccount}</h3>
+			</div>
+			<div className='balanceDisplay'>
+				<h3>Balance: {userBalance}</h3>
+			</div>
+			{errorMessage}
+		</div>
+	);
+}
+
+export default WalletCard;
